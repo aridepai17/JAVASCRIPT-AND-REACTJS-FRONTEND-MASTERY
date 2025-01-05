605653ef42f159ea5f2af77fc12135b2b9c41231
aridepai17 <advaithdepai26@gmail.com>
2025-01-05 10:21:54 +0530

    Operators

---
+++ operators.js
@@ -0,0 +1,58 @@
+// Arithmetic operators
+console.log(2 + 2); // 4
+console.log(42 - 24); // 18
+console.log(2 * 3); // 6
+console.log(10 / 2); // 5
+console.log(10 % 3); // 1
+console.log(10 ** 3); // 1000
+
+// Increment and decrement operators
+let a = 1;
+console.log(a++); // 1
+console.log(a); // 2
+console.log(++a); // 3
+
+let b = 1;
+console.log(b--); // 1
+console.log(b); // 0
+console.log(--b); // -1
+
+// Comparison Operators
+a = 5;
+b = 6;
+console.log(a == b); // false
+console.log(a != b); // true
+console.log(a > b); // false
+console.log(a < b); // true
+console.log(a >= b); // false
+console.log(a <= b); // true
+
+// == vs ===
+a = '6';
+b = 6;
+console.log(a == b); // true
+console.log(a === b); // false
+console.log(a != b); // false
+console.log(a !== b); // true
+
+// Logical Operators
+// Logical OR
+console.log(true || true); // true
+console.log(true || false); // true
+console.log( 2 > 5 || 2 < 5); // true
+console.log(2 == 5 || 2 > 5); // false
+
+// Logical AND
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(2 > 5 && 2 < 5); // false
+console.log( 2 < 3 && 2 > 1); // true
+
+// Logical NOT
+console.log(!true); // false
+console.log(!false); // true
+console.log(!(2 > 5)); // true
+console.log(!(2 < 5)); // false
+
+
+

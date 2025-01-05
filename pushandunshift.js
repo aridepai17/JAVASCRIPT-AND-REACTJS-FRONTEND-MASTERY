e376bf3ca0685ac08b01c7811acc88e6b1c040f6
aridepai17 <advaithdepai26@gmail.com>
2025-01-05 22:11:08 +0530

    push and unshift functions in JavaScript

---
+++ pushandunshift.js
@@ -0,0 +1,9 @@
+// Implement the push and unshift functions
+
+let names = ["John", "Doe", "Jane", "Doe", "James", "Smith"];
+
+names.push("Jenny");
+console.log(names); // ["John", "Doe", "Jane", "Doe", "James", "Smith", "Jenny"]
+
+names.unshift("Jack");
+console.log(names); // ["Jack", "John", "Doe", "Jane", "Doe", "James", "Smith", "Jenny"]

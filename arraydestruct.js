e3d72aee5dc11af764bdd41496a219323ff39559
aridepai17 <advaithdepai26@gmail.com>
2025-01-06 19:27:16 +0530

    Array destructuring

---
+++ arraydestruct.js
@@ -0,0 +1,8 @@
+// Array destructuring implementation
+
+let array1 = [ "Thomas", "Chad", "David" ];
+let item1 = array1[0];
+let item2 = array1[1];
+
+console.log(item1); // Thomas
+console.log(item2); // Chad

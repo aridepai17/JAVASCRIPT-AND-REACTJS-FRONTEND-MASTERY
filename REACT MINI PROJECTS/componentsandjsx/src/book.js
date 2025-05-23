2f22e32231cddf3bac9034816945259786c478e2
aridepai17 <advaithdepai26@gmail.com>
2025-05-23 11:54:43 +0530

    Added props properties to books

--- REACT MINI PROJECTS/componentsandjsx/src/book.js
+++ REACT MINI PROJECTS/componentsandjsx/src/book.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import './book.css'
 
-const book = () => {
-    const image = "https://m.media-amazon.com/images/I/41AH3HNFh0L._SY445_SX342_PQ95_.jpg";
-    const title = "Broken Country";
-    const publisher = "by Clare Leslie Hall";
-    const price = "$100";
+const book = (props) => {
+  const {image, title, publisher, price} = props;
   return (
     <div className="book">
+        {console.log(props)}
         <div className="book-image">
-      <img src={image} alt={title}/>
+      <img src={image}/>
         </div>
         <div className="book-info">
         <h2>{title}</h2>
--- REACT MINI PROJECTS/componentsandjsx/src/booklist.js
+++ REACT MINI PROJECTS/componentsandjsx/src/booklist.js
@@ -1,13 +1,22 @@
-import Book from './book.js'
-import React from 'react'
-
+import Book from "./book.js";
+import React from "react";
+import books from "./books.json"; 
 
 const booklist = () => {
   return (
     <div>
-      <Book/>
+      {books.map((element) => {
+        return (
+          <Book
+            image={element.image}
+            title={element.title}
+            publisher={element.publisher}
+            price={element.price}
+          />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default booklist;
